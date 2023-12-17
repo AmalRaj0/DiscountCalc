93bed027122e007fadea5897659862fd866d604a
AmalRaj0 <amalrajvictus.@gmail.com>
2023-12-17 20:14:09 +0530

    discountcalc

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{padding:"10px",backdropFilter:"blur(15px)",background:"rgba(255, 255, 255, 0.2",outline:"1px solid"}}> 
+      <h2 className='text-center'>𝓢𝓪𝓿𝓲𝓷𝓰𝓼𝓢𝓷𝓪𝓹</h2>
+
+    </div>
+  )
+}
+
+export default Header
